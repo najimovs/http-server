d27184c151a9d90dd6ff8bf0b567eb7e3191a8f2
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-08-08 17:08:43 +0500

    Implement `/todos` & `/posts` endpoints

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log( "OK" )
+import { createServer } from "node:http"
+import { parse } from "url"
+import fs from "fs/promises"
+
+const PORT = 4_000
+
+const server = createServer( async ( req, res ) => {
+
+	const { pathname } = parse( req.url )
+
+	res.writeHead( 200, { "Content-Type": "application/json" } )
+
+	if ( pathname === "/todos" ) {
+
+		const jsonText = await fs.readFile( "./todos.json", "utf-8" )
+
+		res.end( jsonText )
+
+		return
+	}
+	else if ( pathname === "/posts" ) {
+
+		const jsonText = await fs.readFile( "./posts.json", "utf-8" )
+
+		res.end( jsonText )
+
+		return
+	}
+
+	res.end( JSON.stringify( { message: "OK" } ) )
+} )
+
+server.listen( PORT, "127.0.0.1", () => {
+
+	console.log( `Server running at :${ PORT }` )
+} )
